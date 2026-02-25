dbef7bc9f9ba8dccfef07361ddaf68430d0dd379
zzzyxx7 <3040202533@qq.com>
2026-02-25 17:14:58 +0800

    feat(*):性别

--- pojo/src/main/java/com/fuzhou/pojo/vo/AdminUserVO.java
+++ pojo/src/main/java/com/fuzhou/pojo/vo/AdminUserVO.java
@@ -17,6 +17,11 @@ public class AdminUserVO {
          */
         private String name;
 
+    /**
+     * 性别：0=未知，1=男，2=女
+     */
+    private Integer gender;
+
         /**
          * 账号
          */
