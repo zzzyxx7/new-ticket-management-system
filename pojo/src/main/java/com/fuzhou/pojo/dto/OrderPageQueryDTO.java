a5f9770829d491f26259eb2930f5b7995731206d
zzzyxx7 <3040202533@qq.com>
2026-03-01 22:00:06 +0800

    feat(*):把查询订单的编号从模糊匹配改成精确匹配

--- pojo/src/main/java/com/fuzhou/pojo/dto/OrderPageQueryDTO.java
+++ pojo/src/main/java/com/fuzhou/pojo/dto/OrderPageQueryDTO.java
@@ -6,7 +6,7 @@ public class OrderPageQueryDTO {
     private Integer page = 1;          // 页码
     private Integer pageSize = 10;       // 每页数量
     private Integer orderStatus;    // 订单状态（可选筛选）
-    private String orderNo;         // 订单号（可选筛选，模糊查询）
+    private String orderNo;         // 订单号（可选筛选，精确匹配）
     private Long userId;            // 用户ID（可选筛选）
     private String beginTime;       // 开始时间（可选筛选）
     private String endTime;         // 结束时间（可选筛选）
